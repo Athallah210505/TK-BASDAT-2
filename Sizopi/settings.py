28b1d3a0a82a70ae994783b25b5c467d8f8f864a
cleoexcel <cleoxclln@gmail.com>
2025-04-25 19:01:31 +0700

    feat: add FE for rekam_medis_hewan

--- Sizopi/settings.py
+++ Sizopi/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'rekam_medis_hewan',
+    'penjadwalan_pemeriksaan',
+    'pemberian_pakan',
     
 ]
 
@@ -57,7 +61,7 @@ ROOT_URLCONF = 'Sizopi.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
