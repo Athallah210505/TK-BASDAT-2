5b9436f16f670d021fe699cfafde27ab9a281300
Athallah210505 <n4dhif@gmail.com>
2025-04-27 15:31:11 +0700

    done fitur on section biru

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from main.views import show_main
+from main.views import show_main, show_management, show_atraksi_management, show_wahana_management, show_user_booking, show_admin_booking
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
+    path('management/', show_management, name='show_management'),
+    path('atraksi/', show_atraksi_management, name='show_atraksi_management'),
+    path('wahana/', show_wahana_management, name='show_wahana_management'),
+    path('user_booking/', show_user_booking, name='show_user_booking'),
+    path('admin_booking/', show_admin_booking, name='show_admin_booking'),
 ]
--- main/views.py
+++ main/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     return render(request, 'main.html')
+
+def show_management(request):
+    return render(request, 'management.html')
+
+def show_atraksi_management(request):
+    return render(request, 'atraksi_management.html')
+
+def show_wahana_management(request):
+    return render(request, 'wahana_management.html')
+
+def  show_user_booking(request):
+    return render(request, 'user_booking.html')
+
+def show_admin_booking(request):
+    return render(request, 'admin_booking.html')
