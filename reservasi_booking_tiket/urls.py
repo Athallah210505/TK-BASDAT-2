12c70c78afa5274fcbe9a8e43682a34ec1c67c9a
Athallah210505 <n4dhif@gmail.com>
2025-04-29 16:45:40 +0700

    membenarkan atraksi sesuai

--- reservasi_booking_tiket/urls.py
+++ reservasi_booking_tiket/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.show_user_booking, name='show_user_booking'),
     path('admin_booking/', views.show_admin_booking, name='show_admin_booking'),
     path('user_booking/', views.show_user_booking, name='show_user_booking'),
+    path('user_add_booking/', views.show_user_add_booking, name='show_user_add_booking'),
+    path('user_edit_booking/', views.show_user_edit_booking, name='show_user_edit_booking'),
 ]
--- reservasi_booking_tiket/views.py
+++ reservasi_booking_tiket/views.py
@@ -8,3 +8,6 @@ def show_admin_booking(request):
 
 def show_user_add_booking(request):
     return render(request, 'user_add_booking.html')
+
+def show_user_edit_booking(request):
+    return render(request, 'user_edit_booking.html')
